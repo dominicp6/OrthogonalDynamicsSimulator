cabccb32a9a960479d4cd03d54af618b19df5776
dominic <dom.phillips97@gmail.com>
2025-03-24 11:10:31 +0000

    implement split-time integrator

--- ConstrainedDynamicsSimulator/src/molly_integrators.jl
+++ ConstrainedDynamicsSimulator/src/molly_integrators.jl
@@ -35,7 +35,6 @@ function sample_noise_vector!(data)
     end
 end
 
-# TODO: after getting it working in "dimensionless" could try putting back in the units to make sure it still works
 @inline function simulate!(sys,
                            sim::CVConstrainedOverdampedLangevin,
                            n_steps::Integer;
@@ -113,6 +112,136 @@ end
 
     return sys
 end
+
+@inline function find_lambda(φ_grid, x_barₖ, divP, k_bT, friction_nounits, dt, target_CV; tol=1e-6, max_iter=50)
+    # Define the correction term
+    correction = (k_bT * divP ./ friction_nounits) .* dt
+    # Define the function f(λ) = φ_grid(x_barₖ + correction * λ) - target_CV
+    f(λ) = φ_grid(x_barₖ .+ correction .* λ) - target_CV
+    
+    # Initial guess for λ
+    λ = 0.0
+    iter = 0
+    
+    # Newton-Raphson iteration with Zygote for derivative
+    while iter < max_iter
+        # Evaluate function
+        f_val = f(λ)
+        
+        # Check convergence
+        if abs(f_val) < tol
+            return λ, 1
+        end
+        
+        # Compute derivative using Zygote
+        f_deriv = Zygote.gradient(f, λ)[1]  # Gradient returns a tuple, take the first element
+        
+        # Check if derivative is zero or nearly zero to avoid division issues
+        if abs(f_deriv) < 1e-12
+            @warn "Derivative near zero, stopping λ iteration early"
+            return λ, 2
+        end
+        
+        # Update λ
+        λ_new = λ - f_val / f_deriv
+        
+        # Prevent overshooting by limiting step size if needed
+        if abs(λ_new - λ) > 10.0  # Arbitrary large step limit
+            λ_new = λ + sign(λ_new - λ) * 10.0
+        end
+        
+        λ = λ_new
+        iter += 1
+    end
+    
+    return λ, 3
+end
+
+
+@inline function simulate_lambda_correction!(sys,
+    sim::CVConstrainedOverdampedLangevin,
+    n_steps::Integer;
+    n_threads::Integer=Threads.nthreads(),
+    run_loggers=true)
+
+    if length(sys.constraints) > 0
+    @warn "CVConstrainedOverdampedLangevin is not currently compatible with constraints, " *
+    "constraints will be ignored"
+    end
+
+    # Unit assertions 
+    uX, ut, uT, uk, uγ, uM, uF = unit(sys.coords[1][1]), unit(sim.dt), unit(sim.T), unit(sys.k), unit(sim.γ), unit(masses(sys)[1]), sys.force_units
+    @assert uX == u"nm"
+    @assert ut == u"ps"
+    @assert uT == u"K"
+    @assert uk == u"kJ/(mol*K)"
+    @assert uγ == u"ps^-1"
+    @assert uM == u"g/mol"
+    @assert uF == u"kJ/(nm*mol)"
+
+    # Physical quantities
+    masses_nounits = ustrip.(masses(sys))
+    friction_nounits = ustrip(sim.γ)
+    k_bT = ustrip(sys.k) * ustrip(sim.T)   # kJ mol^-1
+    dt = ustrip(sim.dt)                    # ps
+
+    # Coordinate initialisation
+    sys.coords .= wrap_coords.(sys.coords, (sys.boundary,))
+    !iszero(sim.remove_CM_motion) && remove_CM_motion!(sys)
+    neighbors = find_neighbors(sys, sys.neighbor_finder; n_threads=n_threads)
+    apply_loggers!(sys, neighbors, 0, run_loggers; n_threads=n_threads)
+    x_barₖ = ustrip.(sys.coords)           # nm
+    num_coords = length(x_barₖ)
+
+    # Forces and acceleration initialisation
+    forces_nounits_t = ustrip_vec.(similar(sys.coords))   # kJ nm^-1 mol^-1
+    forces_t = forces_nounits_t .* sys.force_units
+    forces_buffer = Molly.init_forces_buffer!(sys, forces_nounits_t, n_threads)
+    accels_nounits_t = forces_nounits_t ./ 1  # we assume an identity mass matrix (in units of g mol^-1)
+    @assert are_units_same((uX / ut^2), uF / uM)        
+
+    # Noise initialisation
+    noise = similar(sys.velocities)
+    noise = sample_noise_vector(num_coords) # dimensionless 
+    noise_scaling = sqrt(2 * k_bT * dt / friction_nounits) / (1^(1/2))  # we assume an identity mass matrix (in units of g mol^-1)
+    # @assert are_units_same(uX, sqrt(uk * uT * ut / uγ) / sqrt(uM))
+
+    # Constrained dynamics initialisation
+    P_M_inv_F, gradφ = compute_Pvec_x(φ_grid=sim.φ_grid, vec=accels_nounits_t, x=x_barₖ)
+    Pnoise = compute_Pvec(gradφ=gradφ, vec=noise)
+    @assert are_units_same(uX, (uF / (uM * uγ)) * ut)
+    divP = compute_divP(φ_flat=sim.φ_flat, gradφ=gradφ, x=x_barₖ)   # nm^-1
+    @assert are_units_same(uX, uk * uT * ut / (uM * uγ * uX))
+
+    # Keep track of constrained CV
+    constrained_CV = sim.φ_grid(x_barₖ)
+    stopping_condition_counts = [0, 0, 0]
+    @show progress = Progress(n_steps)
+    for step_n in 1:n_steps
+        forces_nounits_t .= Molly.forces_nounits!(forces_nounits_t, sys, neighbors, forces_buffer, step_n; n_threads=n_threads)
+        accels_nounits_t .= forces_nounits_t ./ 1  # we assume an identity mass matrix (in units of g mol^-1)
+
+        sample_noise_vector!(noise)
+
+        compute_Pvec_x!(P_M_inv_F, gradφ, φ_grid=sim.φ_grid, vec=accels_nounits_t, x=x_barₖ)
+        divP .= compute_divP(φ_flat=sim.φ_flat, gradφ=gradφ, x=x_barₖ)
+        compute_Pvec!(Pnoise, gradφ=gradφ, vec=noise)
+        x_barₖ .+= ((P_M_inv_F) ./friction_nounits) .* dt .+ noise_scaling .* Pnoise   # no explicit division by m here because we assume an identity mass matrix (in units of g mol^-1)
+        λ, stopping_condition = find_lambda(sim.φ_grid, x_barₖ, divP, k_bT, friction_nounits, dt, constrained_CV)
+        stopping_condition_counts[stopping_condition] += 1
+        x_barₖ .+= (k_bT * divP ./ friction_nounits) .* λ .* dt
+        sys.coords .= wrap_coords.(x_barₖ * uX, (sys.boundary,))
+
+        neighbors = find_neighbors(sys, sys.neighbor_finder, neighbors, step_n; n_threads=n_threads)
+        apply_loggers!(sys, neighbors, step_n, run_loggers; n_threads=n_threads, current_forces=forces_nounits_t * uF)
+
+        next!(progress)
+
+    end
+
+    return sys, stopping_condition_counts
+end
+
 
 
 @inline function simulateOverdamped!(sys,
